fdbdb1a0059f4ffe958d1f2f8eb7c364a6372d9c
Anuj Kumar <54850064+akarj@users.noreply.github.com>
2021-09-09 10:49:19 +0530

    Dropdown shape formed...

--- src/App.js
+++ src/App.js
@@ -14,12 +14,37 @@ function App() {
   return (
     <div className="App">
       <Navbar>
-        <NavItem icon="😃" />
-        <NavItem icon="😃" />
-        <NavItem icon="😃" />
+        <NavItem icon={<PlusIcon />} />
+        <NavItem icon={<BellIcon />} />
+        <NavItem icon={<MessengerIcon />} />
+
+        <NavItem icon={<CaretIcon />}>
+          <DropdownMenu />
+        </NavItem>
       </Navbar>
     </div>
   );
 }
 
 export default App;
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />}
+      ></DropdownItem>
+    </div>
+  );
+}
--- src/components/navItem/NavItem.jsx
+++ src/components/navItem/NavItem.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 export default function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   );
 }
